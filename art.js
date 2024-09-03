616499639f8e1ccfd329341e7606f4fbf93febdb
Elmeerasay <160783904+elmeerasay@users.noreply.github.com>
2024-09-04 02:38:43 +0700

    Update art.js

--- art.js
+++ art.js
@@ -54,7 +54,7 @@ async function swapARTtoUSDT() {
             "0x557ceb8739640b02a547a04643089accb3b88e03",  // ART token
             "0x6cddd437ced8a9af4d53ee51b1645bbd08f9b702"   // USDT token
         ];
-        const amountIn = ethers.utils.parseUnits("1", 18); // 1 ART
+        const amountIn = ethers.utils.parseUnits("4", 18); // 1 ART
         const to = wallet.address; // Recipient address
         const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from now
 
@@ -65,7 +65,7 @@ async function swapARTtoUSDT() {
         const amountOutMin = ethers.utils.parseUnits("0", 18); // Set minimum acceptable amount, could use a slippage tolerance instead
 
         // Send transaction
-        console.log("Sending transaction to swap 1 ART for USDT...");
+        console.log("Sending transaction to swap 4 ART for USDT...");
         const tx = await routerContract.swapExactETHForTokens(
             amountOutMin,
             path,
@@ -78,7 +78,7 @@ async function swapARTtoUSDT() {
         await tx.wait();
         console.log("Transaction confirmed.");
     } catch (error) {
-        console.error("Saldo habis"); //, error);
+        console.error("Saldo Tidak cukup"); //, error);
     }
 }
 
@@ -90,7 +90,7 @@ async function swapUSDTtoART() {
             "0x6cddd437ced8a9af4d53ee51b1645bbd08f9b702",  // USDT token
             "0x557ceb8739640b02a547a04643089accb3b88e03"   // ART token
         ];
-        const amountIn = ethers.utils.parseUnits("100", 18); // 100 USDT
+        const amountIn = ethers.utils.parseUnits("500", 18); // 100 USDT
         const to = wallet.address; // Recipient address
         const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from now
 
@@ -101,7 +101,7 @@ async function swapUSDTtoART() {
         const amountOutMin = ethers.utils.parseUnits("0", 18); // Set minimum acceptable amount, could use a slippage tolerance instead
 
         // Send transaction
-        console.log("Sending transaction to swap 100 USDT for ART...");
+        console.log("Sending transaction to swap 500 USDT for ART...");
         const tx = await routerContract.swapExactTokensForETH(
             amountIn,
             amountOutMin,
@@ -114,7 +114,7 @@ async function swapUSDTtoART() {
         await tx.wait();
         console.log("Transaction confirmed.\n");
     } catch (error) {
-        console.error("Saldo habis"); //, error);
+        console.error("Saldo Tidak cukup"); //, error);
     }
 }
 
@@ -128,10 +128,11 @@ async function performSwaps() {
             await swapUSDTtoART();
             await new Promise(resolve => setTimeout(resolve, 3000)); // Wait for 5 seconds
         } catch (error) {
-            console.error("Saldo habis"); //, error);
+            console.error("Saldo Tidak cukup"); //, error);
             await new Promise(resolve => setTimeout(resolve, 3000)); // Wait for 5 seconds before retrying
         }
     }
 }
 
 performSwaps();
+
